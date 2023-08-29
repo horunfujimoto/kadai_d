983f12a12edfc7ff2607a7fed763b3422d963067
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-29 12:24:16 +0000

    [Add]応用課題3:いいね機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,13 @@ class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :title, presence: true
   validates :body, length: { minimum: 1, maximum: 200 }
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
