5542353a14e0e23e599b1b51d5352e5c548e545f
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-09-04 15:18:36 +0000

    [Add]応用課題6:いいね機能の非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer #同じページに遷移する！！
+    #redirect_to request.referer #同じページに遷移する！！ 非同期化のためコメントアウト
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id) #user_idやpost_idのようなid(主キー)か不明で、別の条件でレコードを検索したい場合はfind_byメソッドを用います
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id) #user_idやpost_idのようなid(主キー)か不明で、別の条件でレコードを検索したい場合はfind_byメソッドを用います
     favorite.destroy
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 end
